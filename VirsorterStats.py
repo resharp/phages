91fd22b826b83b3f7fbbee34bea956e874496217
Callidus <gremmen@resharp.nl>
2019-07-24 09:32:26 +0200

    CHANGE: Added merge of predicted phages with gene content in affi file (gene affinity?). REFAC: Use of VirsorterStats.ini file (not in commit)
    VirsorterStats.ini should look something like:
    
    [DEFAULT]
    
    [output]
    #Directory to be analyzed
    Directory=D:\17 Dutihl Lab\virsorter_output\vs_all_100\

--- VirsorterStats.py
+++ VirsorterStats.py
@@ -1,25 +1,26 @@
 import pandas as pd
 import os
-
-# self.phage_affi_data[self.phage_affi_data.gene_name.str.find("Phage_cluster") == 0].gene_name.value_counts()
+import re
 
 class VirsorterStats:
     phage_signal_filename = ""
-    phage_affi_filename = ""
+    all_affi_filename = ""
+
     phage_data = None
+    all_affi_data = None
     phage_affi_data = None
 
     def __init__(self, mydir):
 
         self.phage_signal_filename = mydir + "VIRSorter_global-phage-signal.csv"
-        self.phage_affi_filename = mydir + "Metric_files\\VIRSorter_affi-contigs.tab"
+        self.all_affi_filename = mydir + "Metric_files\\VIRSorter_affi-contigs.tab"
 
-        if not os.path.isfile(self.phage_affi_filename):
+        if not os.path.isfile(self.all_affi_filename):
             #switch to linux style
-            self.phage_affi_filename = mydir + "Metric_files/VIRSorter_affi-contigs.tab"
+            self.all_affi_filename = mydir + "Metric_files/VIRSorter_affi-contigs.tab"
 
         self.read_phage_data(self.phage_signal_filename)
-        self.read_affi_data(self.phage_affi_filename)
+        self.read_affi_data(self.all_affi_filename)
 
     def read_phage_data(self, phage_signal_filename):
 
@@ -37,20 +38,38 @@ class VirsorterStats:
 
         self.phage_data= pd.read_csv(filename2, delimiter=",")
 
-    def read_affi_data(self, phage_affi_filename):
+    def read_affi_data(self, all_affi_filename):
 
-        f = open(phage_affi_filename, 'r')
+        f = open(all_affi_filename, 'r')
         lines = f.readlines()
         lines_no_comment = [n for n in lines if not n.startswith('>')]
         f.close()
 
-        filename2 = phage_affi_filename + "2"
+        filename2 = all_affi_filename + ".csv"
         f2 = open(filename2, 'w')
         f2.write("gene_id|nr1|nr2|nr3|nr4|gene_name|nr_6|nr_7|nr_8|short_annotation|nr_10|nr_11")
         f2.writelines(lines_no_comment)
         f2.close()
 
-        self.phage_affi_data = pd.read_csv(filename2, delimiter="|")
+        self.all_affi_data = pd.read_csv(filename2, delimiter="|")
+        self.all_affi_data = self.all_affi_data[self.all_affi_data.gene_name != ""]
+        self.all_affi_data = self.all_affi_data[self.all_affi_data.gene_name != "-"]
+
+        list_contigs = list(self.phage_data.Contig_id)
+
+        #add derived field Contig_id for joining
+        self.all_affi_data['Contig_id'] = self.all_affi_data.gene_id.str.replace("\-gene_[0-9]*", "")
+
+        #now join all_affi_data and phage_data on Contig_id
+        self.phage_affi_data = self.all_affi_data.merge(self.phage_data
+                                             , left_on=self.all_affi_data.Contig_id
+                                             , right_on=self.phage_data.Contig_id
+                                             , how='inner')
+        self.phage_affi_data.rename(columns={'key_0':'Contig_id'}, inplace=True)
+
+        #TODO: remove this copy
+        self.phage_affi_data.to_csv(filename2, sep=';', index=False)
+
 
     def category_counts(self):
 
@@ -83,7 +102,12 @@ class VirsorterStats:
 
         return len(self.phage_data[self.phage_data.Complete])
 
+    def all_gene_counts(self):
+        return self.all_affi_data.gene_name.value_counts()
 
+    # def gene_to_contig(self, gene_id):
+    #     contig_id = re.sub("\-gene_[0-9]*", "", gene_id)
+    #     return contig_id
 
-
+# self.phage_affi_data[self.phage_affi_data.gene_name.str.find("Phage_cluster") == 0].gene_name.value_counts()
 
--- VirsorterStatsTest.py
+++ VirsorterStatsTest.py
@@ -1,11 +1,16 @@
 import unittest
 
 from VirsorterStats import VirsorterStats
-
+import configparser
+import random
 
 class VirsorterStatsTest(unittest.TestCase):
 
-    mydir = "D:\\17 Dutihl Lab\\virsorter_output\\vs_all_100\\"
+    config = configparser.ConfigParser()
+    config.read('VirsorterStats.ini')
+
+    mydir = config['output']['Directory']
+
     vs_stats = VirsorterStats(mydir)
 
     def test_vs_stats_non_empty(self):
@@ -14,9 +19,10 @@ class VirsorterStatsTest(unittest.TestCase):
     def test_phage_data_non_empty(self):
         self.assertIsNotNone(self.vs_stats.phage_data)
 
-    def test_phage_data_columns_contains_category(self):
+    def test_phage_data_columns_contains_essential_fields(self):
         cols = self.vs_stats.phage_data.columns.values
         assert 'Category' in cols
+        assert 'Contig_id' in cols
 
     def test_counts_of_category_1_is_2(self):
         assert self.vs_stats.category_counts()['1'] == 2
@@ -34,12 +40,52 @@ class VirsorterStatsTest(unittest.TestCase):
     def test_nr_of_complete_contigs_larger_than_1(self):
         self.assertGreater(self.vs_stats.nr_of_complete_contigs(), 1)
 
-# test that the affi only contains non-empty genes
-# we are only interested in the annotations
+    def test_affi_contains_non_empty_genes(self):
+
+        sub_list_empty_genes = self.vs_stats.all_affi_data[self.vs_stats.all_affi_data.gene_name == ""]
+
+        self.assertEqual(len(sub_list_empty_genes), 0)
+
+        sub_list_dash_genes = self.vs_stats.all_affi_data[self.vs_stats.all_affi_data.gene_name == "-"]
+        self.assertEqual(len(sub_list_dash_genes), 0)
+
+        # print("Length of filtered affi contig file:")
+        # print(len(self.vs_stats.all_affi_data))
+
+    #this method probably is going to confuse
+    #because we are not looking at the shared genes (yet)
+    def test_top_gene_is_individual_gene(self):
+        best_gene = self.vs_stats.all_gene_counts().index[0]
+        print("The most abundant gene in all contigs (not just phages)")
+        print(best_gene)
+        self.assertEqual(best_gene.find("gi_"), 0)
+
+    # def test_convert_gene_id_to_contig_id(self):
+    #
+    #     gene_id = "VIRSorter_PQXB01000001___PQXB01000001_1___[ANME-1_cluster_archaeon_strain_G37ANME1___2056316_3]-gene_8"
+    #     contig_id = "VIRSorter_PQXB01000001___PQXB01000001_1___[ANME-1_cluster_archaeon_strain_G37ANME1___2056316_3]"
+    #
+    #     self.assertEqual(self.vs_stats.gene_to_contig(gene_id), contig_id)
+
+    def test_phage_affi_data_columns_contains_essential_fields(self):
+        cols = self.vs_stats.phage_affi_data.columns.values
+
+        assert 'key_0' not in cols
+        assert 'Contig_id' in cols
+
+    def test_phage_affi_contains_contigs_with_predicted_phages(self):
+
+        phage_contigs = list(self.vs_stats.phage_data.Contig_id)
+
+        list_of_contigs_in_affi = list(self.vs_stats.phage_affi_data.Contig_id)
+
+        for i in range(0,5):
+            random_affi_contig = random.choice(list_of_contigs_in_affi)
+            # print(random_affi_contig)
+            assert random_affi_contig in phage_contigs
+
 # also join the simple annotations from the affi file
 # look at the top Phage clusters and genes of the shared files
-
-# extract contigs
 
 if __name__ == '__main__':
     unittest.main()
