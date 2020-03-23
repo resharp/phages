7f56227fafd06798d73ef690b3d64e415c142227
resharp <gremmen@resharp.nl>
2020-03-23 13:25:57 +0100

    IMPROVE [micro diversity] calculate linear regression measures for entropy dependence on depth on two levels, on codon level and on gene level, for one ref genome

--- MakeDiversityPlots.py
+++ MakeDiversityPlots.py
@@ -5,11 +5,13 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns;sns.set()
+from scipy.stats import linregress
 
 
 # MakeDiversityPlots
 # 1. plot entropy against average coverage for genes and for all AA positions over all samples
 #   for different age categories
+# 2. determine significance of trend lines (and significance of distributions?)
 class MakeDiversityPlots:
 
     sample_dir = ""
@@ -17,6 +19,7 @@ class MakeDiversityPlots:
     plot_dir = ""
 
     aa_df = None
+    protein_df = None
 
     def __init__(self, sample_dir, ref):
 
@@ -69,16 +72,18 @@ class MakeDiversityPlots:
 
         os.makedirs(self.plot_dir, exist_ok=True)
 
-    def make_plots(self):
+    def aggregate_on_protein_level(self):
 
-        protein_df = self.aa_df.groupby(["age_cat", "protein"]).agg(
+        self.protein_df = self.aa_df.groupby(["age_cat", "protein"]).agg(
             {'coverage': 'mean',
              'entropy': 'mean'}).reset_index()
+
+    def make_plots(self):
 
         title = "crAssphage: mean entropy of all position against mean coverage for that position."
         self.plot_entropy_for_age_categories(self.aa_df, "codon", title)
         title = "crAssphage: mean entropy of all genes against mean coverage for that gene."
-        self.plot_entropy_for_age_categories(protein_df, "protein", title)
+        self.plot_entropy_for_age_categories(self.protein_df, "protein", title)
 
     def plot_entropy_for_age_categories(self, data, level, title):
 
@@ -90,22 +95,49 @@ class MakeDiversityPlots:
                         data=data)
         plt.title(title)
 
-        filename=self.plot_dir + self.dir_sep + "entropy_against_coverage_for_{level}.svg".format(level=level)
+        filename="{}{}entropy_against_coverage_for_{level}_{ref}.svg".format(
+            self.plot_dir, self.dir_sep, level=level, ref=self.ref)
 
-        plt.show()
-        # plt.savefig(filename)
+        plt.savefig(filename)
         plt.clf()
 
     def write_measures(self):
+        # https://stackoverflow.com/questions/33490833/display-regression-equation-in-seaborn-regplot
 
         # to do: write measures for the slope of the lines for different age categories
         # and determine if they are significantly different
+        self.write_measures_for_level(self.protein_df, "protein")
+        self.write_measures_for_level(self.aa_df, "codon")
 
-        pass
+    def write_measures_for_level(self, data, level):
+
+        df_lr = pd.DataFrame(columns=('ref', 'slope', 'intercept', 'r_value', 'p_value', 'std_err'))
+        df_lr.index.name = 'age_cat'
+
+        age_cats = data.age_cat.unique()
+
+        for age_cat in age_cats:
+
+            data_age = data[data.age_cat == age_cat]
+
+            x_data = data_age.coverage
+            y_data = data_age.entropy
+
+            slope, intercept, r_value, p_value, std_err = linregress(x=x_data, y=y_data)
+
+            df_lr.loc[age_cat] = [
+                self.ref, slope, intercept, r_value, p_value, std_err
+            ]
+
+        filename = "{}{}linear_regression_for_{level}_{ref}.txt".format(
+            self.plot_dir, self.dir_sep, level=level, ref=ref)
+        df_lr.to_csv(path_or_buf=filename, sep='\t')
 
     def do_analysis(self):
 
         self.read_files()
+
+        self.aggregate_on_protein_level()
 
         self.create_plot_dir()
 
@@ -144,3 +176,7 @@ sample_dir = r"D:\17 Dutihl Lab\_tools\_pipeline\ERP005989"
 # or run one sample, or a list of
 ref = "crassphage_refseq"
 do_analysis(["-d", sample_dir, "-r", ref])
+
+# refs = ["crassphage_refseq", "sib1_ms_5", "err975045_s_1", "inf125_s_2", "srr4295175_ms_5"]
+# for ref in refs:
+#     do_analysis(["-d", sample_dir, "-r", ref])
